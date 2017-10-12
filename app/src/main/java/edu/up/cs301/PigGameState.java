825dab77b0c47652f9e952c298090676d1d4bf7a
alexaruiz <ruiza20@up.edu>
2017-10-12 16:36:05 -0700

    all

--- app/src/main/java/edu/up/cs301/PigGameState.java
+++ app/src/main/java/edu/up/cs301/PigGameState.java
@@ -1,8 +1,26 @@
 package edu.up.cs301;
 
+import edu.up.cs301.game.infoMsg.GameState;
+
 /**
  * Created by alexaruiz on 10/12/17.
  */
 
-public class PigGameState {
+
+public class PigGameState extends GameState{
+
+    public int player0;
+    public int player1;
+    public int total;
+    public int die;
+
+    public PigGameState(){
+
+
+
+    }
+
+
+
+
 }
