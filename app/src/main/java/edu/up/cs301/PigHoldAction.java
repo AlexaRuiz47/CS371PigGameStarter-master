931cbf4f1eb8f8a1f2fc54f2578d4099e12ec917
plaisted20 <plaisted20@up.edu>
2017-10-12 16:59:45 -0700

    pig hold and roll

--- app/src/main/java/edu/up/cs301/PigHoldAction.java
+++ app/src/main/java/edu/up/cs301/PigHoldAction.java
@@ -1,8 +1,16 @@
 package edu.up.cs301;
 
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
 /**
  * Created by alexaruiz on 10/12/17.
  */
 
-public class PigHoldAction {
+public class PigHoldAction extends GameAction {
+
+    public PigHoldAction(GamePlayer p) {
+        super(p);
+    }
+
 }
--- app/src/main/java/edu/up/cs301/PigRollAction.java
+++ app/src/main/java/edu/up/cs301/PigRollAction.java
@@ -1,8 +1,16 @@
 package edu.up.cs301;
 
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
 /**
  * Created by alexaruiz on 10/12/17.
  */
 
-public class PigRollAction {
+public class PigRollAction extends GameAction {
+
+
+    public PigRollAction(GamePlayer p) {
+        super(p);
+    }
 }
